da7d45698a3593c29032166403eddaa49acbc8b2
Meena Ravichandran <111807454+meenaravichandran1@users.noreply.github.com>
2025-01-20 12:33:09 -0800

    Update main.tf

--- sample-2/main.tf
+++ sample-2/main.tf
@@ -17,6 +17,6 @@ resource "aws_instance" "my_ec2" {
   ami           = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.small"
   tags = {
-    Name = "ExampleAppServerInstance-Uri-7"
+    Name = "Example"
   }
 }
